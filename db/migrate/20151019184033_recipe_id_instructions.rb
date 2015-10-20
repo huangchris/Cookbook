d2472504685ee1827d08ee30d73bd96ded9bb440
Christopher Huang <CCHuang90@gmail.com>
2015-10-20 15:45:43 -0700

    try to fix production db

--- db/migrate/20151019184033_recipe_id_instructions.rb
+++ db/migrate/20151019184033_recipe_id_instructions.rb
@@ -1,7 +1,10 @@
 class RecipeIdInstructions < ActiveRecord::Migration
   def change
-    add_column :instructions, :recipe_id, :integer, null: false
-    add_foreign_key :instructions, :recipes
-    add_index :instructions, :recipe_id
+    # add_column :instructions, :recipe_id, :integer, null: false
+    # add_foreign_key :instructions, :recipes
+    # add_index :instructions, :recipe_id
   end
 end
+
+# yeah, I edited this later; for some reason this column didn't generate in
+# development, but now it's a problem on production.
